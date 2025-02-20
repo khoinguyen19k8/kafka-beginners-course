cd5f672fda9e9dda44101e7a551c83c1f1b9d71b
Khoi <khoinguyen19k8@gmail.com>
2025-02-20 17:18:53 +0100

    Add dependencies and create Producer Demo class

---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -0,0 +1,21 @@
+package io.conduktor.demos.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Hello World");
+
+        // create Producer Properties
+
+        // create the Producer
+
+        // send data
+
+        // flush and close the producer
+
+    }
+}
